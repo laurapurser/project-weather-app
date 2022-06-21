7108f46eb66ec27de61d2f29e7c4ad6926496125
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-06-21 20:14:12 +0100

    Added Weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
