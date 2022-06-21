3ef87795406bd52ed3d5ac65c5029f72d2c152ed
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-06-21 19:53:37 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://objective-einstein-d849c5.netlify.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Laura Purser
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/laurapurser/react-weather-app-new"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
