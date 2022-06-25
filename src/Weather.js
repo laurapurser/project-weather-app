ce9b5b41bf4250126b49042885b24cd0c5b39edb
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-06-25 19:44:34 +0100

    Changed img to animated external component

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       windspeed: response.data.wind.speed,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
   }
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,11 +17,13 @@ export default function WeatherInfo(props) {
       <div className="row results">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <span className="float-left">
+              <WeatherIcon
+                code={props.data.icon}
+                alt-text={props.data.description}
+              />
+            </span>
+
             <span className="float-left">
               <WeatherTemperature celsius={props.data.temp} />
             </span>
