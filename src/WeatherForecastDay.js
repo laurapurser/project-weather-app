cf48a2e5a5c5bdd2a2109740e5641e414c103422
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-06-27 11:14:38 +0100

    Removed Date function to check limit

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -10,16 +10,16 @@ export default function WeatherForecastDay(props) {
     let temperature = Math.round(props.data.temp.min);
     return `${temperature}`;
   }
-  function setDay() {
-    let date = new Date(props.data.dt * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
-    return days[setDay()];
-  }
+  //function setDay() {
+  //let date = new Date(props.data.dt * 1000);
+  // let day = date.getDay();
+  //let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+  // return days[setDay()];
+  //}
 
   return (
     <div className="WeatherForecastDay">
-      <div className="forecast-day">{setDay()}</div>
+      <div className="forecast-day">{props.data.dt}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={42} />
       <div className="forecast-temps">
         <span className="forecast-max">{maxTemp()}°</span>
